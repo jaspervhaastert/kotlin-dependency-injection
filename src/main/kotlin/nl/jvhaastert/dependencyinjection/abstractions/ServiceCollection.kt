497f2efc7bd1a09c6301904e1aa4d459d253134a
Jasper van Haastert <jaspervhaastert@outlook.com>
2021-01-26 22:50:39 +0100

    Add KDocs for service collection and service provider abstractions

--- src/main/kotlin/nl/jvhaastert/dependencyinjection/abstractions/ServiceCollection.kt
+++ src/main/kotlin/nl/jvhaastert/dependencyinjection/abstractions/ServiceCollection.kt
@@ -6,14 +6,47 @@ import nl.jvhaastert.dependencyinjection.implementations.ServiceCollection as Se
 
 public interface ServiceCollection {
 
+    /**
+     * Gets the [ServiceSupplier] for the given service class.
+     *
+     * @param[serviceClass] The service class for which to get the [ServiceSupplier].
+     * @return The [ServiceSupplier] when found or else `null`.
+     */
     public fun <T> getSupplier(serviceClass: Class<T>): ServiceSupplier<T>?
 
+    /**
+     * Adds a singleton [ServiceSupplier] with the given instance.
+     *
+     * @param[serviceClass] The class for which to add a [ServiceSupplier].
+     * @param[instance] The instance to add.
+     */
     public fun <T> addSingleton(serviceClass: Class<T>, instance: T)
+
+    /**
+     * Adds a lazy singleton [ServiceSupplier] with the given factory.
+     *
+     * @param[serviceClass] The class for which to add a [ServiceSupplier].
+     * @param[factory] The factory to add.
+     */
     public fun <T> addSingleton(serviceClass: Class<T>, factory: Factory<T>)
+
+    /**
+     * Adds a factory [ServiceSupplier].
+     *
+     * @param[serviceClass] The class for which to add a [ServiceSupplier].
+     * @param[factory] The factory to add.
+     */
     public fun <T> addFactory(serviceClass: Class<T>, factory: Factory<T>)
 
     public companion object {
+
+        /**
+         * Creates a default [ServiceCollection].
+         *
+         * @return A new [ServiceCollection] instance.
+         */
         public fun create(): ServiceCollection = ServiceCollectionImpl()
+
     }
 
 }
--- src/main/kotlin/nl/jvhaastert/dependencyinjection/abstractions/ServiceProvider.kt
+++ src/main/kotlin/nl/jvhaastert/dependencyinjection/abstractions/ServiceProvider.kt
@@ -1,15 +1,39 @@
 package nl.jvhaastert.dependencyinjection.abstractions
 
+import nl.jvhaastert.dependencyinjection.exceptions.NoServiceSupplierFoundException
+import nl.jvhaastert.dependencyinjection.models.ServiceSupplier
 import nl.jvhaastert.dependencyinjection.implementations.ServiceProvider as ServiceProviderImpl
 
 public interface ServiceProvider {
 
+    /**
+     * Tries to get an instance for the given service class.
+     *
+     * @param[serviceClass] The service class for which to get an instance.
+     * @return An instance of [serviceClass] or null when no [ServiceSupplier] is found.
+     */
     public fun <T> getOrNull(serviceClass: Class<T>): T?
+
+    /**
+     * Gets an instance for the given service class.
+     *
+     * @param[serviceClass] The service class for which to get an instance.
+     * @return An instance of the service class
+     * @throws[NoServiceSupplierFoundException] When no [ServiceSupplier] for the given [serviceClass] can be found.
+     */
     public fun <T> get(serviceClass: Class<T>): T
 
     public companion object {
+
+        /**
+         * Creates a default [ServiceProvider].
+         *
+         * @param[serviceCollection] The [ServiceCollection] to create the [ServiceProvider] with.
+         * @return A new [ServiceProvider] instance.
+         */
         public fun create(serviceCollection: ServiceCollection): ServiceProvider =
             ServiceProviderImpl(serviceCollection)
+
     }
 
 }
