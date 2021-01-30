50564ff5412c62ef5eb4b08cc5a3427f0d7c7810
Jasper van Haastert <jaspervhaastert@outlook.com>
2021-01-30 13:25:05 +0100

    Add extension functions for get all functions

--- src/main/kotlin/nl/jvhaastert/dependencyinjection/extensions/ServiceProviderExtensions.kt
+++ src/main/kotlin/nl/jvhaastert/dependencyinjection/extensions/ServiceProviderExtensions.kt
@@ -2,5 +2,6 @@ package nl.jvhaastert.dependencyinjection.extensions
 
 import nl.jvhaastert.dependencyinjection.abstractions.ServiceProvider
 
+public inline fun <reified T> ServiceProvider.getAll(): List<T> = getAll(T::class.java)
 public inline fun <reified T> ServiceProvider.getOrNull(): T? = getOrNull(T::class.java)
 public inline fun <reified T> ServiceProvider.get(): T = get(T::class.java)
